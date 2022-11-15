b0db0cf98083541cb7fcf5357805ae09097ba734
BTSBRINGMEHERE <mynameisyyb@naver.com>
2022-11-15 14:18:41 +0900

    Update App
    1. 사용자 입력시 정답 맞추기 기능 추가
    2. 정답 기록남기는 시스템 추가 및 정답을 맞추고 나서 새로운 게임 만들기 추가
    3. 유효성 검사
      - 숫자 이외의 문자 입력 금지
      - 4자리 숫자만 입력 가능
      - 중복된 숫자 입력 금지

--- src/App.js
+++ src/App.js
@@ -5,30 +5,105 @@ import generateRandomNumber from "./random"
 function App() {
 	const [randomNumber, setRandomNumber] = useState(generateRandomNumber())
 	const [answer, setAnswer] = useState("")
+	const [logs, setLogs] = useState([])
+	const [isSuccess, setIsSuccess] = useState(false)
 
-	useEffect(() => {}, [randomNumber])
+	useEffect(() => {
+		console.log(randomNumber)
+	}, [randomNumber])
 
 	const handleAnswerChanged = (event) => {
 		const {
 			target: { value },
 		} = event
-
 		setAnswer(value)
+	}
+
+	const handleSubmit = () => {
+		const ans = answer.split("").map((item) => Number(item))
+
+		if (ans.some((number) => isNaN(number))) {
+			alert("숫자만 입력해주세요")
+			return
+		}
+
+		if (ans.length != 4) {
+			alert("4자리 숫자만 입력해주세요")
+			return
+		}
+
+		const isDuplicate = ans.some((number) => {
+			return ans.indexOf(number) !== ans.lastIndexOf(number)
+		})
+
+		if (isDuplicate) {
+			alert("중복된 값이 있습니다")
+			return
+		}
+
+		const { strike, ball } = randomNumber.reduce(
+			(prev, cur, index) => {
+				// 같은 자리에 같은 수가 존재하면 스트라이크
+				if (ans[index] === cur) {
+					return {
+						...prev,
+						strike: prev.strike + 1,
+					}
+				}
+				// 다른 자리에 수가 존재하면 볼
+				if (ans.includes(cur)) {
+					return {
+						...prev,
+						ball: prev.ball + 1,
+					}
+				}
+
+				return prev
+			},
+			{ strike: 0, ball: 0 }
+		)
+
+		if (strike === 4) {
+			alert("ㅊㅋㅊㅋㅊㅋㅊㅋ")
+			setLogs([...logs, `${answer} (축하합니다. 정답입니다!!)`])
+			setIsSuccess(true)
+			return
+		}
+
+		setLogs([...logs, `${answer} (strike: ${strike}, ball: ${ball})`])
+	}
+
+	const handleRetry = () => {
+		setRandomNumber(generateRandomNumber())
+		setAnswer("")
+		setLogs([])
+		setIsSuccess(false)
 	}
 
 	return (
 		<div className="App">
 			<h1>숫자 야구 게임</h1>
-			<header className="header">{randomNumber}</header>
+			<header className="header">
+				{isSuccess ? `정답: ${answer}` : "----"}
+			</header>
 			<section>
-				<input type="text" value={answer} onChange={handleAnswerChanged} />
-				<button>맞춰보기</button>
+				<input
+					type="text"
+					value={answer}
+					onChange={handleAnswerChanged}
+					disabled={isSuccess}
+				/>
+				{isSuccess ? (
+					<button onClick={handleRetry}>다시하기</button>
+				) : (
+					<button onClick={handleSubmit}>맞춰보기</button>
+				)}
 			</section>
 			<h2>기록</h2>
 			<ol>
-				<li>1234 (strike: 0, ball: 2)</li>
-				<li>5678 (strike: 0, ball: 2)</li>
-				<li>7890 (strike: 0, ball: 2)</li>
+				{logs.map((log, index) => {
+					return <li key={`${log}_${index}`}>{log}</li>
+				})}
 			</ol>
 		</div>
 	)
